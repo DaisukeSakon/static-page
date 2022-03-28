4fa7c7d6bee268db934be14a839556c9c3c9324a
DaisukeSakon <Dsakon0250@outlook.jp>
2022-03-28 07:55:42 +0000

    finish Rails Tutorial all

--- app/models/user.rb
+++ app/models/user.rb
@@ -77,14 +77,13 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end  
   
-  # 試作feedの定義
-  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  # ユーザーのステータスフィードを返す
   def feed
-    following_ids = "SELECT followed_id FROM relationships
-                     WHERE follower_id = :user_id"
-    Micropost.where("user_id IN (#{following_ids})
-                     OR user_id = :user_id", user_id: id)
-  end 
+    part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    Micropost.left_outer_joins(user: :followers)
+             .where(part_of_feed, { id: id }).distinct
+             .includes(:user, image_attachment: :blob)
+  end
   
   # ユーザーをフォローする
   def follow(other_user)
